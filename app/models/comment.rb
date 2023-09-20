949be88ee6488597ea1452074c3e1b507c444e57
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-20 20:31:37 +0500

    Add method to update comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def self.update_comment_counter(post_id)
+    post = Post.find_by(id: post_id)
+    post.update(comments_counter: post.comments.count)
+  end
 end
