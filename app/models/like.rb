c25827bc3e6d0174a8aadc0ecdf302513d5aad69
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-20 20:31:48 +0500

    Add method to update like counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  def self.update_like_counter(post_id)
+    post = Post.find_by(id: post_id)
+    post.update(likes_counter: post.likes.count)
+  end
 end
