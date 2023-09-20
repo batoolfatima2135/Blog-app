9c6be758026117ba0873ccff3fd10b701bb1e3be
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-20 20:33:05 +0500

    Add method to fetch recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: "author_id"
+
+   def self.recent_posts(id)
+    user = User.find_by(id: id)
+    user.posts.order(created_at: :desc).limit(3)
+  end
 end
