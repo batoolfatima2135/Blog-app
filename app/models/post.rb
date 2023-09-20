d3e36519ba9a71f1248cd007f5a657b38fefb13c
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-20 20:32:51 +0500

    Add methods: update post counter , fetch comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: "author_id"
+  has_many :likes, foreign_key: "post_id"
+  has_many :comments, foreign_key: "post_id"
+  
+  def self.update_post_counter(user_id)
+    user = User.find_by(id: user_id)
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def self.recent_comments(post_id)
+    post = Post.find_by(id: post_id)
+    post.comments.order(created_at: :desc).limit(5)
+  end
+ 
 end
