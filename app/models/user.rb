34c17e5d0742c5fe276a515fb7220772d3028979
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-20 21:29:59 +0500

    Add has many relationship for likes and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'user_id'
+  has_many :likes, foreign_key: 'user_id'
 
   def self.recent_posts(id)
     user = User.find_by(id:)
