6a53583a6740f8fc4206ef788e2e7801693e87c3
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-21 21:50:10 +0500

    Write test for post model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ it 'update likes_counter of post' do
+    user = User.new(name: "Mark")
+    post = Post.create(title: 'Test Post', user: user)
+    like1 = Like.create(post: post, user: user)
+    like2 = Like.create(post: post, user: user)
+    like3 = Like.create(post: post, user: user)
+    like4 = Like.create(post: post, user: user)
+    
+    expect(post.likes_counter).to equal(4)
+  end
+
 end
