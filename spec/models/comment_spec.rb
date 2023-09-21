626e1fdc0904b287e44a3d8cb6c57cc4f779b897
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-21 21:50:01 +0500

    Write test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   it 'update comments_counter of post' do
+    user = User.new(name: "Mark")
+    post = Post.create(title: 'Test Post', user: user, text: 'Hi Tom!')
+    comment1 = Comment.create(post: post, user: user, text: 'Hi Tom!')
+    comment2 = Comment.create(post: post, user: user, text: 'Hi Tom!')
+    comment3 = Comment.create(post: post, user: user, text: 'Hi Tom!')
+    comment4 = Comment.create(post: post, user: user, text: 'Hi Tom!')
+    
+    expect(post.comments_counter).to equal(4)
+  end
 end
