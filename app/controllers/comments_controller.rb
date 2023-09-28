cf004cd460bef9e1c5e87fd6721d7fb7fa7758ce
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-29 01:44:08 +0500

    Add create method for likes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user # Assuming you have user authentication in place
+
+    if @comment.save
+      flash[:success] = "Comment created successfully!"
+      redirect_to user_post_path(@user, @post)
+    else
+      flash[:error] = "Comment could not be saved."
+      redirect_to user_post_path(@user, @post)
+    end
+  end
+
+  private
+
+  def comment_params
+  params.require(:comment).permit(:text) # Permit :text instead of :content
+end
+end
