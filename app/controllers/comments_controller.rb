210987df95e85dd1f5218a9980369c830be1e2f6
Batool Fatima <batoolfatima2135@gmail.com>
2023-09-29 12:59:23 +0500

    Solve Rubocop offenses

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,13 @@
 class CommentsController < ApplicationController
-
   def new
     @comment = Comment.new
   end
-  
+
   def create
     @user = current_user
     @comment = @user.comments.build(comment_params)
     @comment.post_id = params[:post_id]
-    
+
     if @comment.save
       flash[:success] = 'Comment created successfully!'
     else
@@ -20,6 +19,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:text) 
+    params.require(:comment).permit(:text)
   end
 end
