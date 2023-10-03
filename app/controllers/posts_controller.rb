b24420c9ae5948c6d31c7f6c13793c0ac80d9980
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-04 00:10:10 +0500

    Add authentication to controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+   before_action :authenticate_user!, only: [:new, :create]
   def show
     @post = Post.find(params[:id])
     @index = params[:index]
