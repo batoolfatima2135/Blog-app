d1fe7357adf6acff20446fda53b80060b727605c
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-05 21:49:38 +0500

    Add routes for Api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,9 +7,21 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts do
-      resources :comments,  only: [:new, :create, :destroy]
-      resources :likes,  only: [:create] 
+      resources :posts do
+        resources :comments,  only: [:new, :create, :destroy]
+        resources :likes,  only: [:create] 
+      end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index]
+      end
+      resources :posts do
+          resources :comments,  only: [:index, :create]
+      end
     end
   end
 end
